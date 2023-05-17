a14e50803b0b3635ba9e8e6480e63a1c6cedaa30
GabrielReimer <115717098+GabrielReimer@users.noreply.github.com>
2023-05-17 17:02:52 +0200

    please

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import "/./style.css";
-import * as THREE from "three";
+//import * as THREE from "three";
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+//import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { GLTFLoader } from "/js/GLTFLoader.js";
 const loader = new GLTFLoader();
 
 loader.load(
